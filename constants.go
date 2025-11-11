6b33b50f13ca40e9e5f3df605eba3391e46b5bd7
viciouscal <78273450+viciouscal@users.noreply.github.com>
2025-11-12 02:34:42 +0300

    Update constants.go

--- constants.go
+++ constants.go
@@ -11,12 +11,12 @@ import (
 	"vencordinstaller/buildinfo"
 )
 
-const ReleaseUrl = "https://api.github.com/repos/Vendicated/Vencord/releases/latest"
-const ReleaseUrlFallback = "https://vencord.dev/releases/vencord"
-const InstallerReleaseUrl = "https://api.github.com/repos/Vencord/Installer/releases/latest"
-const InstallerReleaseUrlFallback = "https://vencord.dev/releases/installer"
+const ReleaseUrl = "https://api.github.com/repos/viciouscal/Vencord/releases/latest"
+const ReleaseUrlFallback = "https://api.github.com/repos/viciouscal/Vencord/releases/latest"
+const InstallerReleaseUrl = "https://api.github.com/repos/viciouscal/Installer/releases/latest"
+const InstallerReleaseUrlFallback = "https://api.github.com/repos/viciouscal/Installer/releases/latest"
 
-var UserAgent = "VencordInstaller/" + buildinfo.InstallerGitHash + " (https://github.com/Vencord/Installer)"
+var UserAgent = "VencordInstaller/" + buildinfo.InstallerGitHash + " (https://github.com/viciouscal/Installer)"
 
 var (
 	DiscordGreen  = color.RGBA{R: 0x2D, G: 0x7C, B: 0x46, A: 0xFF}
